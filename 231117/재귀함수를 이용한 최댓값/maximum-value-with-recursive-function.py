a2398831731c606b9d8e4ab889649de5f1852434
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-17 10:42:00 +0900

    Title: 재귀함수를 이용한 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231117/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,9 @@
+n=int(input())-1
+list_name=list(map(int,input().split()))
+def f(list_name,n,max_value=0):
+    if n==-1:
+        return max_value
+    if list_name[n]>max_value:
+        max_value=list_name[n]
+    return f(list_name,n-1,max_value)
+print(f(list_name,n,max_value))
