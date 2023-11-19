aced3f4a146fd97f45d8dade413633d03c03ceda
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-19 10:32:41 +0900

    Title: 나누고 빼면서 합하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231119/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 231119/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -2,14 +2,14 @@ n,m=map(int,input().split())
 arr=list(map(int,input().split()))
 
 def f(arr,m):
-    a_sum=arr[m]
+    a_sum=
     while m>1:
         if m%2==0:
+            a_sum+=arr[m-1]
             m//=2
-            a_sum+=arr[m]
         else:
+            a_sum+=arr[m-1]
             m-=1
-            a_sum+=arr[m]
-    a_sum+=arr[m]
+    a_sum+=arr[m-1]
     return a_sum
 print(f(arr,m))
