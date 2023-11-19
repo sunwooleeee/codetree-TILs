b3447f4cf4d51d4d7afa3bccc49a83809f375c62
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-19 10:43:05 +0900

    Title: 특정 구간의 합, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231119/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,8 @@
+n,m=map(int,input().split())
+an=list(map(int,input().split()))
+sum=0
+for i in range(m):
+    x,y=map(int,input().split())
+    for j in range(x-1,y):
+        sum+=an[j]
+print(sum)
