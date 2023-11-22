97048638a2700e49fda2c5ca7de6f8e1aaf13bbb
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-22 17:03:20 +0900

    Title: 국영수 순이지, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/국영수 순이지/korean-english-math-order.py
+++ 231122/국영수 순이지/korean-english-math-order.py
@@ -1,14 +1,14 @@
-student_list=[]
-class student:
+n=int(input())
+arr=[]
+class s:
     def __init__(self,name,kor,eng,math):
         self.name=name
         self.kor=kor
         self.eng=eng
         self.math=math
-n=int(input())
 for i in range(n):
     name,kor,eng,math=tuple(input().split())
-    student_list.append(student(name,int(kor),int(eng),int(math)))
-student_list.sort(key=lambda x:(-x.kor,-x.eng,-x.math))
-for i in student_list:
-    print(i.name,i.kor,i.eng,i.math)
+    arr.append(s(name,int(kor),int(eng),int(math)))
+arr.sort(key=lambda x:(-x.kor,-x.eng,-x.math))
+for i in range(n):
+    print(arr[i].name,arr[i].kor,arr[i].eng,arr[i].math)
