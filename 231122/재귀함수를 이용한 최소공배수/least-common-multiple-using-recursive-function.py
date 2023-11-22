5a57ece02422337af3b1906830110f329ac4e433
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-22 11:06:25 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231122/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,23 @@
+n=int(input())-1
+arr=list(map(int,input().split()))
+
+
+def f2(a,b): #두 수의 최대공약수 계산
+    if a<b:
+        a,b=b,a
+    if b==0:
+        return a
+    else:
+        return f2(b,a%b)
+
+def f1(a,b): # 두 수의 최소공배수 계싼
+    return a*b//f2(a,b)
+c=f1(arr[n-1],arr[n])
+def f(arr,n,c):
+    
+    if n==1:
+        return c
+    else:
+        return f(arr,n-1,f1(arr[n-2],c))
+               
+print(f(arr,n,c))
