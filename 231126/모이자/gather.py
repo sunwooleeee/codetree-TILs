f4bff8c664cfd4118bd6f54974f9ef8564e691f7
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-26 11:01:12 +0900

    Title: 모이자, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231126/모이자/gather.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr=list(map(int,input().split()))
+min_value=99999
+for k in range(n):
+    sum_dif=0
+    for j in range(n):
+        sum_dif+=arr[j]*abs(k-j)
+    min_value=min(min_value,sum_dif)
+print(min_value)
