1805e53e422ac7f5e69a38587b2042f4bcfd3688
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-28 13:26:24 +0900

    Title: 변수 값 교체하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231128/변수 값 교체하기 3/replacing-variable-values-3.java
+++ 231128/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -3,7 +3,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a,b; a=3;b=5;
         a,b=b,a;
-        System.out.println(a)
-        System.out.print(b)
+        System.out.println(a);
+        System.out.print(b);
     }
 }
