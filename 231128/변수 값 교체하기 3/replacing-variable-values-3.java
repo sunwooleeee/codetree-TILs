63bfa78bf0bb0f0c4c0202145f71df55a3c30b31
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-28 13:27:24 +0900

    Title: 변수 값 교체하기 3, Time: 103ms, Memory: 8MB, Status: Passed - CodeTree

--- 231128/변수 값 교체하기 3/replacing-variable-values-3.java
+++ 231128/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a,b; a=3;b=5;
+        int temp;
         temp=a;
         a=b;
         b=temp;
