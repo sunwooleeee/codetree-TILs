fd3a784c4bf65cb65b8fa180d6e38897bad257a9
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-28 13:16:06 +0900

    Title: 길이 단위 변환하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231128/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a=9.2;
+        double b=1.3;
+        System.out.printf("%.1fft = %.1fcm\n",a,a*30.48);
+        System.out.printf("%.1fmi = %.1fcm",b,b*160934);
+
+    }
+}
