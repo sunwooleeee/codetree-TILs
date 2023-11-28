339102d70fcfc68649a5dab52d45eed0a67ac680
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-28 13:31:37 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231128/데이터 교환/exchange-data.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a,b,c,temp_a,temp_b;
+        a=5;b=6;c=7;
+        temp_a=a;
+        temp_b=b;
+        a=c;
+        b=temp_a;
+        c=temp_b;
+        System.out.println(a)
+        System.out.println(b)
+        System.out.println(c)
+    }
+}
