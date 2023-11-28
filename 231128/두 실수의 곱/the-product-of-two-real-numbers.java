09fd27cc15b872c6c15d8da2ee66c198f9dfdcc7
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-28 13:17:35 +0900

    Title: 두 실수의 곱, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

--- 231128/두 실수의 곱/the-product-of-two-real-numbers.java
+++ 231128/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         double a,b;
-        a=5.26
-        b=8.27
+        a=5.26;
+        b=8.27;
         System.out.printf("%.3f",a*b);
     }
 }
