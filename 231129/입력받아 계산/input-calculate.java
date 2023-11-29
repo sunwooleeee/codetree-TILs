f6c2662a2579f68d738f5cd624e1e7e93751ae53
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-29 12:13:17 +0900

    Title: 입력받아 계산, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 231129/입력받아 계산/input-calculate.java
+++ 231129/입력받아 계산/input-calculate.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 
-        scanner sc=new scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         System.out.print(a+2);
     }
