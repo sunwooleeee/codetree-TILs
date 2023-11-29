4b3965443ef5d81b336345d6cf0e1786c8edba77
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-29 12:12:26 +0900

    Title: 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231129/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 
+        scanner sc=new scanner(System.in);
+        int a=sc.nextInt();
+        System.out.print(a+2);
+    }
+}
