ed372204cdde29882bfaa4f627873b07adfa9fa8
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-30 16:39:21 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231130/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,11 @@
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+arr=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
+m1, d1, m2, d2=map(int,input().split())
+
+def cal(m,d):
+    if m==0:
+        return d
+    d+=num_of_days[m]
+    m-=1
+    return cal(m,d)
+print(arr[cal(m2,d2)-cal(m1,d1)])
