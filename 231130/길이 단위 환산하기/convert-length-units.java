480be472df49a9b1f743479efbbab3122266cea1
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-11-30 12:34:21 +0900

    Title: 길이 단위 환산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231130/길이 단위 환산하기/convert-length-units.java
+++ 231130/길이 단위 환산하기/convert-length-units.java
@@ -2,8 +2,8 @@ import java.util.Scanner
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc=new Scanner(System.in)
+        Scanner sc=new Scanner(System.in);
         double a=sc.nextDouble();
-        System.out.printf("%.1f",a*30.48)
+        System.out.printf("%.1f",a*30.48);
     }
 }
