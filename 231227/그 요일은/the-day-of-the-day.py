aeb565573a4b0158789433390dde46e35fcde66e
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-27 17:36:43 +0900

    Title: 그 요일은, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231227/그 요일은/the-day-of-the-day.py
+++ 231227/그 요일은/the-day-of-the-day.py
@@ -2,12 +2,11 @@ m1,d1,m2,d2=map(int,input().split())
 a=input()
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 arr=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
-day1=d1
 for i in range(m1):
-    day1+=num_of_days[i]
-day2=d2
+    d1+=num_of_days[i]
 for i in range(m2):
-    day2+=num_of_days[i]
-day1+=arr.index(a)
-dif=day2-day1
-print(dif//7)
+    d2+=num_of_days[i]
+print(d1,d2)
+d1+=arr.index(a)
+dif=d2-d1
+print(dif//7+1)
