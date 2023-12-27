7bae711d9c545eeb829d4d8b51931f82abbc303b
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-27 16:21:21 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231227/다양하게 출력/print-in-variety.c
+++ 231227/다양하게 출력/print-in-variety.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
-    printf("Total days in Year\n%d\nCircumference rate\n3.145926535",365);
+    printf("Total days in Year\n%d\nCircumference rate\n3.1415926535",365);
     return 0;
 }
