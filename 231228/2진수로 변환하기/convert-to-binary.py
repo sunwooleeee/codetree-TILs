db3b323d4bbaf2e56080f4a3a7bbacf77f8f916c
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-28 15:12:44 +0900

    Title: 2진수로 변환하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231228/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=[]
+def f(n,arr):
+    if n==1:
+        arr.insert(0,1)
+        n=len(arr)
+        for i in range(n):
+            print(arr[i],end="")
+        return 
+    arr.insert(0,n%2)
+    n=n//2
+    f(n,arr)
+
+f(n,arr)
