fc2e2d15868f9af631534d6022197648cd2ea621
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-28 14:32:58 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/데이터 교환/exchange-data.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=5,b=6,c=7;
+    int temp=a;
+    a=c;
+    c=b;
+    b=temp;
+    printf("%d\n%d\n%d",a,b,c);
+    return 0;
+}
