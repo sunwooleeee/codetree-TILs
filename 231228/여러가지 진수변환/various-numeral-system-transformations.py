ae6717cbd8eae60d7b85d88b18a14123fc2c4a98
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-28 15:55:06 +0900

    Title: 여러가지 진수변환, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/여러가지 진수변환/various-numeral-system-transformations.py
+++ 231228/여러가지 진수변환/various-numeral-system-transformations.py
@@ -1,15 +1,23 @@
 n,b=map(int,input().split())
 if b==4:
     arr=[]
-    while n>=4:
-        arr.insert(0,n%4)
-        n//=4
-    arr.insert(0,n)
-    print(*arr,sep="")
+    while True:
+        if n>=4:
+            arr.append(n%4)
+            n//=4
+        else:
+            arr.append(n)
+            break;
+    for i in arr[::-1]:
+        print(i,end="")
 if b==8:
     arr=[]
-    while n>=8:
-            arr.insert(0,n%8)
-            n//4
-    arr.insert(0,n)
-    print(*arr,sep="")
+    while True:
+        if n>=8:
+            arr.append(n%8)
+            n//=8
+        else:
+            arr.append(n)
+            break;
+    for i in arr[::-1]:
+        print(i,end="")
