63619c98001b09227be723f1011b114a30b7426b
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-29 22:00:35 +0900

    Title: 합과 평균의 차, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231229/합과 평균의 차/sub-of-average-and-sum.c
+++ 231229/합과 평균의 차/sub-of-average-and-sum.c
@@ -4,6 +4,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c;
     scanf("%d%d%d"&a,&b,&c);
-    printf("%d\n%d\n%d",a+b+c,(a+b+c)/3,(a+b+c)-(a+b+c)/3)
+    printf("%d\n%d\n%d",a+b+c,(a+b+c)/3,(a+b+c)-(a+b+c)/3);
     return 0;
 }
