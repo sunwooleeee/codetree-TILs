b918d9e670269ebb09f8ec37a9e89a45ad169132
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-30 11:37:17 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231230/체질량지수/bmi.c
+++ 231230/체질량지수/bmi.c
@@ -4,9 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
     scanf("%d%d",&a,&b);
+    int bmi;
     bmi=b/(a*a);
     printf("%d",bmi);
-    if bmi>=25 {
+    if (bmi>=25) {
         printf("Obesity");
     }
     return 0;
