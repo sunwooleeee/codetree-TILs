7371d4b0b3a5fdd794fbd5dcb23c53ebaf62c22e
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2023-12-30 11:36:25 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231230/체질량지수/bmi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    bmi=b/(a*a);
+    printf("%d",bmi);
+    if bmi>=25 {
+        printf("Obesity");
+    }
+    return 0;
+}
