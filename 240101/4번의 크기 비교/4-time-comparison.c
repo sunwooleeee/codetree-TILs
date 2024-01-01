6284e991f0b1a93ff3a5677464e0a8717d5ee4b9
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 20:59:38 +0900

    Title: 4번의 크기 비교, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240101/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+    int b,c,d,e;
+    scanf("%d%d%d%d",b,c,d,e);
+    a>b ? printf("1\n") : printf("0\n");
+    a>c ? printf("1\n") : printf("0\n");
+    a>d ? printf("1\n") : printf("0\n");
+    a>e ? printf("1\n") : printf("0\n");
+    
+    return 0;
+}
