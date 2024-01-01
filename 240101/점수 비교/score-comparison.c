77049ee8044484c7c2dba13dac81ef41c9f0c992
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 21:01:43 +0900

    Title: 점수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240101/점수 비교/score-comparison.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a1,a2;
+    int b1,b2;
+    if (a1>b1 && a2>b2){
+        printf("1");
+    }
+    else[
+        printf("0");
+    ]
+    return 0;
+}
