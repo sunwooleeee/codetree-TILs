6393d773f7b2ecbdb378f578a0bc10c193bc2dd1
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 19:55:04 +0900

    Title: 두 번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/두 번의 연산/two-operations.c
+++ 240101/두 번의 연산/two-operations.c
@@ -4,10 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     scanf("%d",&a);
-    if a%2!=0{
+    if (a%2!=0){
         a+=3;
     }
-    if a%3==0{
+    if (a%3==0){
         a/=3;
     }
     printf("%d",a);
