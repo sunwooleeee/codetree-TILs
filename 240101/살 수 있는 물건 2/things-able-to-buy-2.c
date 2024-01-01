fa4d2a49da081dfb9cc5ba4d617e104f3d3a50ac
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 19:40:04 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240101/살 수 있는 물건 2/things-able-to-buy-2.c
+++ 240101/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -4,10 +4,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d",&n);
-    if n>=3000{
+    if (n>=3000){
         printf("book");
     }
-    else if n>1000{
+    else if (n>=1000){
         printf("mask");
     }
     else {
