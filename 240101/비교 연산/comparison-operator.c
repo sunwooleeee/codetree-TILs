8556d5d8f85e394530024c6d7dfb3c7ca8e25ed8
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 20:16:50 +0900

    Title: 비교 연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240101/비교 연산/comparison-operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    (a>=b) ? printf("1\n") :printf("0\n");
+    (a>b) ? printf("1\n") :printf("0\n");
+    (b>=a) ? printf("1\n") :printf("0\n");
+    (b>a) ? printf("1\n") :printf("0\n");
+    (a==b) ? printf("1\n") :printf("0\n");
+    (a!=b) ? printf("1\n") :printf("0\n");
+
+    
+
+    return 0;
+}
