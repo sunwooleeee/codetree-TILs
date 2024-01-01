90bdd491cbcffa34efbf4821330338ab76301dde
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 20:07:48 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
+++ 240101/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -5,16 +5,16 @@ int main() {
     int a,b;
     scanf("%d%d",&a,&b);
     if (a<b){
-        printf("1");
+        printf("1 ");
     }
     else{
-        printf("0");
+        printf("0 ");
     }
     if (a==b){
-        printf("1");
+        printf("1 ");
     }
     else{
-        printf("0");
+        printf("0 ");
     }
     return 0;
 }
