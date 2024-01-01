145f5e4179f6ed07c61d036fd171a8bcc1be7847
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 20:17:17 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/비교 연산/comparison-operator.c
+++ 240101/비교 연산/comparison-operator.c
@@ -2,6 +2,8 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d%d",&a,&b);
     (a>=b) ? printf("1\n") :printf("0\n");
     (a>b) ? printf("1\n") :printf("0\n");
     (b>=a) ? printf("1\n") :printf("0\n");
