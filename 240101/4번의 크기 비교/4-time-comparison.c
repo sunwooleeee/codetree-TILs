450093c99a55d51cd957053e21aa19a04603d2d4
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-01 20:59:53 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/4번의 크기 비교/4-time-comparison.c
+++ 240101/4번의 크기 비교/4-time-comparison.c
@@ -5,7 +5,7 @@ int main() {
     int a;
     scanf("%d",&a);
     int b,c,d,e;
-    scanf("%d%d%d%d",b,c,d,e);
+    scanf("%d%d%d%d",&b,&c,&d,&e);
     a>b ? printf("1\n") : printf("0\n");
     a>c ? printf("1\n") : printf("0\n");
     a>d ? printf("1\n") : printf("0\n");
