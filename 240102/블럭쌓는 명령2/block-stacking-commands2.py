3a5201063d4310e4306cf74d1cded4c96f097d23
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-02 13:44:03 +0900

    Title: 블럭쌓는 명령2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240102/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,9 @@
+n,k=map(int,input().split())
+arr=[]
+for i in range(n):
+    arr.append(0)
+for i in range(k):
+    a,b=map(int,input().split())
+    for j in range(a-1,b):
+        arr[j]+=1
+    print(arr)
