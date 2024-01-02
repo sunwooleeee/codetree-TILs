ee69a9ba58d00cbcf9fdf8dbb89527c275cfb19a
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-02 13:34:23 +0900

    Title: 진수 to 진수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240102/진수 to 진수/transformation-of-number-system.py
+++ 240102/진수 to 진수/transformation-of-number-system.py
@@ -1,20 +1,20 @@
 a,b=map(int,input().split())
 n=input()
 def f1(n,a):
-    arr=[]
     num=0
+    arr=[]
     n=str(n)
     for i in range(len(n)):
         arr.append(int(n[i]))
-    for j in arr[::-1]:
-        num=num*a+j
-    return num
+    for i in arr:
+        num=num*a+i
+    return(num)
 def f2(n,b):
     arr=[]
     while n>=b:
         arr.append(n%b)
-        n//=b
+        n=n//b
     arr.append(n)
     print(*arr[::-1],sep="")
-    return
+    return 
 f2(f1(n,a),b)
