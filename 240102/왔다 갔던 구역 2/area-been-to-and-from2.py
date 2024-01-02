409e35800e4efbeb2355346fb3b188efd234d288
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-02 15:25:17 +0900

    Title: 왔다 갔던 구역 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240102/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,26 @@
+n=int(input())
+arr=[0 for i in range(1001)]
+temp=501
+for i in range(n):
+    x,y=tuple(input().split())
+    x=int(x)
+    if y=='R':
+        arr[temp]+=1
+        for j in range(x):
+            temp+=1
+            arr[temp]+=1
+    else:
+        arr[temp]+=1
+        for j in range(x):
+            temp-=1
+            arr[temp]+=1
+cnt=0
+for i in range(1,1001):
+    if arr[i-1]<2 and arr[i]>=2:
+        temp=i
+    
+    elif arr[i]>=2 and arr[i+1]<2:
+        dif=i-temp
+        cnt+=dif
+    
+print(cnt)
