f4026babb4545cb774a1e6027dc295b8233ab5f3
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 20:55:39 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/369 게임/369-game.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        if (i%3==0){
+            printf("0 ");
+        }
+        else if (i>=10){
+            if ((i-(i/10)*10==3) || (i-(i/10)*10==6) || (i-(i/10)*10==9)){
+                printf("0 ");
+            }
+            else{
+                if ((i/10==3) || (i/10==6) || (i/10==9)){
+                    printf("0 ");
+                }
+            }
+        }
+        else{
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
