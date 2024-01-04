ee889dcfa32a5693e68e6586b45da50a46b28f4d
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 17:05:39 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/n번 더하기/add-n-times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,n;
+    scanf("%d%d",&a,&n);
+    for (int i;i<n;i++){
+        a+=n;
+        printf("%d\n",a);
+    }
+    return 0;
+}
