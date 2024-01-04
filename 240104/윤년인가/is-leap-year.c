ff364b34ff1c886a1b297b8008ec7a02dc708c28
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 14:12:32 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/윤년인가/is-leap-year.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+    scanf("%d",&y);
+    if (y%4==0){
+        if (y%100==0){
+            if (y%400==0){
+                printf("true");
+            }
+            else{
+                printf("false");
+            }
+        }
+        else{
+            printf("true");
+        }
+    }
+    else{
+        printf("false");
+    }
+    return 0;
+}
