18a1106fe1e9fa2681b5024abf776dfb79d9bb3c
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 15:48:40 +0900

    Title: 홀수만 출력 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/홀수만 출력 2/output-only-odd-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if (b%2==0){
+        b-=1;
+    }
+    for (b;b>=a;b--){
+        printf("%d ",b);
+    }
+    return 0;
+}
