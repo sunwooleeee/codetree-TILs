02fd3eed220e80e4fea6aca893c0ca303fc444b0
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 14:17:32 +0900

    Title: 일 수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240104/일 수 구하기/number-of-days-in-month.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+    if (a==2){
+        printf("28");
+    }
+    else if (a<=7){
+        if (a%2==0){
+            printf("30");
+        }
+        else{
+            printf("31");
+        }
+    }
+    else{
+        if (a%2==0){
+            printf("31");
+        
+        }
+    }   else{
+        printf("30");
+    }
+    return 0;
+}
