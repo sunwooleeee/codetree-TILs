d38f51981be1736db9a9029fb4c2d6cf6be58eb3
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 12:25:08 +0900

    Title: 그 요일은, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240104/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,16 @@
+m1,d1,m2,d2=map(int,input().split())
+a=input()
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+arr=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
+while m1>0:
+    m1-=1
+    d1+=num_of_days[m1]
+while m2>0:
+    m2-=1
+    d2+=num_of_days[m2]
+d1+=arr.index(a)
+cnt=1
+while d1+7<=d2:
+    cnt+=1
+    d1+=7
+print(cnt)
