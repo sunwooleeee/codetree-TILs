6a24d634c997e1c59f9a51d65d33164b0c6b0dcd
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-04 14:27:25 +0900

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c;
+    int n;
+    scanf("%c %d",&c,&n);
+    if (c=='Y'){
+        if (n>=37){
+            printf("A");
+        }
+        else{
+            printf("C");
+        }
+    }
+    else{
+        if (n>=37){
+            printf("B");
+        }
+        else{
+            printf("D");
+        }
+    }
+    scanf("%c %d",&c,&n);
+    if (c=='Y'){
+        if (n>=37){
+            printf("A");
+        }
+        else{
+            printf("C");
+        }
+    }
+    else{
+        if (n>=37){
+            printf("B");
+        }
+        else{
+            printf("D");
+        }
+    }
+    scanf("%c %d",&c,&n);
+    if (c=='Y'){
+        if (n>=37){
+            printf("A");
+        }
+        else{
+            printf("C");
+        }
+    }
+    else{
+        if (n>=37){
+            printf("B");
+        }
+        else{
+            printf("D");
+        }
+    }
+    return 0;
+}
