e91e5e702406bf276bdd092974a9e42d34c5c6cb
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 16:20:43 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/1부터 곱셈하기/multiple-from-one.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int prod=1;
+    scanf("%d",&n);
+    for (int i=1;i<=10;i++){
+        prod*=i;
+        if (prod>=n){
+            printf("%d",i);
+            break;
+        }
+    }
+    return 0;
+}
