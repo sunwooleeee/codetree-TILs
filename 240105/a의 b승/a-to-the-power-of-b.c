904eacae076bb03af79b662377c8d66071c069c6
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 14:42:54 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/a의 b승/a-to-the-power-of-b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int prod=1;
+    for (int i=1;i<=b;i++){
+        prod*=a;
+    }
+    printf("%d",prod);
+    return 0;
+}
