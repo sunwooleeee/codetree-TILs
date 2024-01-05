4fb5365832b7eb18966495a337e404a7fbb29ec2
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 22:34:00 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240105/0 나올때까지 출력/print-until-zero-is-given.c
+++ 240105/0 나올때까지 출력/print-until-zero-is-given.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int a;
-    while (true){
+    while(true){
         scanf("%d",&a);
         if (a==0){
             break;
