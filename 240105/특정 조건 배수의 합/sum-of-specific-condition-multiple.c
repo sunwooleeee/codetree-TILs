3a7693df2519132db47eee46cd9bec0bdf9f1fbf
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 14:13:58 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/특정 조건 배수의 합/sum-of-specific-condition-multiple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    int sum=0;
+    scanf("%d%d",&a,&b);
+    for (a;a<=b;a++){
+        if (a%5==0){
+            sum+=a;
+        }
+    }
+    return 0;
+}
