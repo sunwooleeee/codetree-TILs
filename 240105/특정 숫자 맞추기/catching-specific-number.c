351fd3f3abb2d8a73c0ad49207df8f3585c6b75c
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 22:36:33 +0900

    Title: 특정 숫자 맞추기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240105/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdiobool.h>
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    while (true){
+        if (n>25){
+            printf("Higher");
+        }
+        else if (n<25){
+            printf("Lower");
+        }
+        else{
+            printf("Good");
+        }
+    }
+
+    return 0;
+}
