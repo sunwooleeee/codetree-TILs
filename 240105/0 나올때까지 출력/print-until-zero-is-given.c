f70d4a69a9fb0a08844844303a0e8a306a538fcd
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 22:32:41 +0900

    Title: 0 나올때까지 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240105/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    while (true){
+        scanf("%d",&a);
+        if (a==0){
+            break;
+        }
+        printf("%d\n",a);
+    }
+    return 0;
+}
