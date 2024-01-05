cbe8e65d67d2df2918b282b9315b98e209526391
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-05 13:14:39 +0900

    Title: 등장횟수 확인, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/등장횟수 확인/check-number-of-appearances.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int cnt=0;
+    for (int i=1;i<=5;i++){
+        scanf("%d",&a);
+        if (a%2==0){
+            cnt+=1;
+        }
+    }
+    return 0;
+}
