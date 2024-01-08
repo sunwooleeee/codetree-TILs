b6f69be9cb75dfb983e1b2cc3df3988ce07b7117
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-08 13:52:51 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/별표 출력하기 6/print-star-6.c
+++ 240108/별표 출력하기 6/print-star-6.c
@@ -14,7 +14,7 @@ int main() {
         printf("\n");
     }
     for (int i=1;i<=n-1;i++){
-        for (int j=1;j<=n-2*(i-1);j++){
+        for (int j=1;j<=2*(n-1-i);j++){
             printf(" ");
         }
         for (int j=1;j<=1+2*i;j++){
