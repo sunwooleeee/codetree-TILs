da071cf7b1692417238632b976c19a240cac494d
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-08 16:11:38 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/별 그리기/star-drawing.c
+++ 240108/별 그리기/star-drawing.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d",&n);
-    for (int i=1;i<=3;i++){
+    for (int i=1;i<=n;i++){
         for (int j=1;j<=n-i;j++){
             printf(" ");
         }
