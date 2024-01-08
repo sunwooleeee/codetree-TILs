683a594628bc0db17d1124e55b7e09a0c7d7ba45
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-08 15:43:48 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/별표 출력하기 10/print-star-10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        if (i%2!=0){
+            for (int j=1;j<=(i+1)/2;j++){
+                printf("* ");
+            }
+            printf("\n");
+        }
+        else{
+            for (int k=1;k<=n-(i-2)/2;k++){
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    for (int i=n;i>=1;i--){
+        if (i%2!=0){
+            for (int j=1;j<=(i+1)/2;j++){
+                printf("* ");
+            }
+            printf("\n");
+        }
+        else{
+            for (int k=1;k<=n-(i-2)/2;k++){
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
