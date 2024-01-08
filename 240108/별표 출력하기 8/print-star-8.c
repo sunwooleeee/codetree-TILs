c59e62d7c4887c816aa8465832dc72f6e2d30510
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-08 15:11:04 +0900

    Title: 별표 출력하기 8, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/별표 출력하기 8/print-star-8.c
+++ 240108/별표 출력하기 8/print-star-8.c
@@ -5,17 +5,18 @@ int main() {
     int n;
     scanf("%d",&n);
     for (int i=1;i<=n;i++){
-        if (i%2==0){
+        if (i%2!=0){
+            printf("*\n");
+        }
+        else{
             for (int j=1;j<=i;j++){
                 printf("* ");
             }
             printf("\n");
         }
-        else{
-            printf("*");
-            printf("\n");
-        }
+
+    }
         
     return 0;
-}
+
 }
