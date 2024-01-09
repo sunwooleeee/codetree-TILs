a00b0a971ec5cdb7bac198d18633abef8eb4ef83
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-09 12:11:02 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/행과 열/row-column.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for (int i=1;i<=a;i++){
+        for (int j=1;j<=b;j++){
+            printf("%d ",j*i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
