ff39791bb4ab5a989efce7743c7f28282a31717d
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-10 14:03:23 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/좌우 지그재그 출력/print-lr-zigzag.c
+++ 240110/좌우 지그재그 출력/print-lr-zigzag.c
@@ -4,25 +4,19 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d",&n);
-    int cnt=1;
+    
     for (int i=1;i<=n;i++){
         for (int j=1;j<=n;j++){
             if (i%2==0){
-                printf("%d ",cnt);
-                cnt-=1;
+                printf("%d ",i*n-j+1);
             }
             else{
-                printf("%d ",cnt);
-                cnt+=1;
+                printf("%d ",(i-1)*n+j);
+                
             }
         }
-        if (i%2==0){
-            cnt+=n+1;
-        }
-        else{
-            cnt+=n-1;
-        }
         printf("\n");
+        
     }
     return 0;
 }
