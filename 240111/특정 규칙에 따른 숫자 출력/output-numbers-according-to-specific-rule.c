8fc0000d4d51c3e20596a998b8f13c69a613755c
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-11 11:38:31 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt=0;
+    scanf("%d",&n);
+    for ( int i=n;i>=1;i--){
+        if (cnt==9){
+            cnt=0;
+        }
+        
+        for (int j=1;j<=n-i;j++){
+            printf("  ");
+        }
+        for (int j=i;j>=1;j--){
+            cnt+=1;
+            printf("%d ",cnt);
+        }
+        printf("\n");
+    }
+    return 0;
+}
