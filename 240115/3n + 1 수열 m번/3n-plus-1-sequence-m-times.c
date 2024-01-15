3efd362e2c958b53daa8bea7741f6c520a6c4abc
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-15 13:45:28 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
+++ 240115/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -2,13 +2,15 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int cnt=0;
+    
     int n;
     int m;
     scanf("%d",&m);
     for (int i=1;i<=m;i++){
     scanf("%d",&n);
+    int cnt=0;
     while (n!=1){
+        
         if (n%2==0){
             n=n/2;
             cnt+=1;
