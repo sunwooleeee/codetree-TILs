92d0f89ec00c0fe785d5f218ce51d9ecc5a177f8
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-16 13:06:19 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240116/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int sum_val=0;
+    for (int i=0;i<10;i++){
+        scanf("%d",&arr[i]);
+        sum_val+=arr[i];
+    }
+    printf("%d",sum_val);
+    return 0;
+}
