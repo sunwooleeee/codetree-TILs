d02ff25c90177558b9aa09a8a78f6b15d266c8f5
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-17 13:45:25 +0900

    Title: 수열 조작, Time: 314ms, Memory: 34MB, Status: Passed - CodeTree

--- 240117/수열 조작/sequence-manipulation.py
+++ 240117/수열 조작/sequence-manipulation.py
@@ -1,6 +1,7 @@
 n=int(input())
-arr=[i for i in range(1,n+1)]
-for j in range(n-1):
-    arr.pop(0)
-    arr.append(arr.pop(0))
-print(*arr)
+from collections import deque
+dq=deque(i for i in range(1,n+1))
+while len(dq)>1:
+    dq.popleft()
+    dq.append(dq.popleft())
+print(dq.pop())
