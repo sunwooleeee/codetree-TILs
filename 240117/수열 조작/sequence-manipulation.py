6ed2140470200e99e28a20d1d3e06a17d597badd
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-17 13:41:11 +0900

    Title: 수열 조작, Time: 5043ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240117/수열 조작/sequence-manipulation.py
+++ 240117/수열 조작/sequence-manipulation.py
@@ -1,7 +1,6 @@
 n=int(input())
 arr=[i for i in range(1,n+1)]
-while len(arr)!=1:
+for j in range(n-1):
     arr.pop(0)
-    arr.append(arr[0])
-    arr.pop(0)
+    arr.append(arr.pop(0))
 print(*arr)
