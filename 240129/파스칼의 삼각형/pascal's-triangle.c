2f52de1e62d8f6991fbd802258ba617bc662261e
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-29 13:27:38 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/파스칼의 삼각형/pascal's-triangle.c
+++ 240129/파스칼의 삼각형/pascal's-triangle.c
@@ -10,6 +10,9 @@ int main() {
             if ((i==0) || (j==0)) {
                 arr[i][j]=1;
             }
+            else if (i==j){
+                arr[i][j]=1;
+            }
             else{
                 arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
             }
