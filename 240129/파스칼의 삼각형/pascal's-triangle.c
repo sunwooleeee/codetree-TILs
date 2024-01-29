d18b08c5c3004e6e7eed410d7481ce3db90ca346
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-01-29 13:23:39 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240129/파스칼의 삼각형/pascal's-triangle.c
+++ 240129/파스칼의 삼각형/pascal's-triangle.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d",&n);
     for (int i=0;i<n;i++){
         for (int j=0;j<=i;j++){
-            if (i==0 || j==0) {
+            if ((i==0) || (j==0)) {
                 arr[i][j]=1;
             }
             else{
