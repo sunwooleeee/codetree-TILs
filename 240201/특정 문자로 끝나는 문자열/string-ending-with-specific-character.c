a8e9790be62d51562bf3d55459c5b1e2211b4463
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-02-01 15:15:24 +0900

    Title: 특정 문자로 끝나는 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240201/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
+++ 240201/특정 문자로 끝나는 문자열/string-ending-with-specific-character.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <bool.h>
 int main() {
     // 여기에 코드를 작성해주세요.
+    bool TF=true;
     char s;
     char arr[10][100];
     for (int i=0;i<10;i++){
@@ -13,7 +15,11 @@ int main() {
     for (int i=0;i<10;i++){
         if ((arr[i][strlen(arr[i])-1])==s){
             printf("%s\n",arr[i]);
+            TF=false
         }
+    }
+    if (TF){
+        printf("None")
     }
     return 0;
 }
