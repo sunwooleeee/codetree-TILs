0275b90cefbc63a8e106a1899fcb047b2041dc70
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-02-06 00:20:53 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240206/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
+++ 240206/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
@@ -2,15 +2,31 @@
 #include <string.h>
 int main() {
     // 여기에 코드를 작성해주세요.
-    char a[101],b[101];
+    char a[300],b[300];
     scanf(" %s %s",&a,&b);
-    char aa[300]=strcat(a,b);
-    char bb[300]=strcat(b,a);
-    if (aa==bb){
+    int n=strlen(a);
+    int m=strlen(b);
+    for (int i=0;i<=n;i++){
+        if (i==n){
+            b[m+i]='\0';
+            break;
+        }
+        b[m+i]=a[i];
+    }
+    
+    for (int i=0;i<=m;i++){
+        if (i==m){
+            a[n+i]='\0';
+            break;
+        }
+        a[n+i]=b[i];
+    }
+    if (a==b){
         printf("true");
     }
     else{
-        printf("else");
+        printf("false");
     }
+    printf("\n%s\n%s",a,b);
     return 0;
 }
