338a446176bef28c500dc4e15bed47379a349585
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-02-15 11:08:01 +0900

    Title: 실수 입력받아 계산 2, Time: 161ms, Memory: 11MB, Status: Passed - CodeTree

--- 240215/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 240215/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -4,6 +4,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc=new Scanner(System.in);
         double a=sc.nextDouble();
-        Syste,.out.printf("%.2f",a+1.5);
+        System.out.printf("%.2f",a+1.5);
     }
 }
