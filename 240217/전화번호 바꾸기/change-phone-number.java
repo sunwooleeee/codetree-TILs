2a7012408bf7f465902dec347acb5ccc7a93b048
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-02-17 13:35:30 +0900

    Title: 전화번호 바꾸기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240217/전화번호 바꾸기/change-phone-number.java
+++ 240217/전화번호 바꾸기/change-phone-number.java
@@ -4,6 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc=new Scanner(System.in);
         sc.useDelimiter("-");
+        int z=sc.nextInt()
         int x=sc.nextInt();
         int y=sc.nextInt();
         System.out.printf("010-%d-%d",y,x);
