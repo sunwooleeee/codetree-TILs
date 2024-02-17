b893d1280b7dbfe6a176d7bf9561bea59f73a1d9
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-02-17 13:34:47 +0900

    Title: 전화번호 바꾸기, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240217/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        sc.useDelimiter("-");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.printf("010-%d-%d",y,x);
+    }
+}
