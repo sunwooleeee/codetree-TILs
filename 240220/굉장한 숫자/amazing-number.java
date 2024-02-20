de97b25dafd87017efed819cb22c604054bab658
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2024-02-20 21:20:37 +0900

    Title: 굉장한 숫자, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240220/굉장한 숫자/amazing-number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if (((n%2!=0) && (n%3==0)) || ((n%2==0) && (n%5==0))){
+            System.out.printf("true");
+        }
+        else{
+            System.out.printf("false");
+        }
+    }
+}
