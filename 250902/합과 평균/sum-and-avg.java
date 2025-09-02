fe20749b9f0d4ecba891dbe64aab92be3cb3e717
sunwooleeee <151102136+sunwooleeee@users.noreply.github.com>
2025-09-02 19:13:51 +0900

    Title: 합과 평균, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/합과 평균/sum-and-avg.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        double c= (a+b);
+        c=c/2;
+        System.out.println((a+b) +" "+ c );
+    }
+}
